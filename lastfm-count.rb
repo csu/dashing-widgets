f5dd963dac478c29f08682eeaf7330a3c23f0eea
Christopher Su <christophersu9@gmail.com>
2015-03-03 18:00:36 -0800

    fic

--- lastfm-count.rb
+++ lastfm-count.rb
@@ -3,12 +3,13 @@ require "json"
 require "date"
 
 lastfm_api_key = 'YOUR_LASTFM_API_KEY_HERE'
+lastfm_username = 'christophersu'
 
 SCHEDULER.every '30s' do
     today = Date.today.to_time.utc.to_i
     tomorrow = today + 24*60*60
     
-    uri = URI.parse("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=christophersu&api_key=#{lastfm_api_key}&format=json&limit=200&from=#{today}&to=#{tomorrow}")
+    uri = URI.parse("http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=#{lastfm_username}&api_key=#{lastfm_api_key}&format=json&limit=200&from=#{today}&to=#{tomorrow}")
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
