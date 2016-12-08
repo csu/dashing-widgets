5dc0bec7791ef69379cfc12962a131c014f34ad3
Christopher Su <hello@christopher.su>
2016-12-08 02:01:09 -0800

    update todoist api

--- todoist-completed.rb
+++ todoist-completed.rb
@@ -3,8 +3,8 @@ require "json"
 
 todoist_token = 'YOUR_TODOIST_TOKEN_HERE'
 
-SCHEDULER.every '30s' do
-    uri = URI.parse("http://api.todoist.com/API/getProductivityStats?token=#{todoist_token}")
+SCHEDULER.every '3m' do
+    uri = URI.parse("https://todoist.com/API/v7/completed/get_stats?token=#{todoist_token}")
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
